00d22311361811ed22db322521553ae6e2f9c1ff
Daniel Lung <daniel.w.lung@gmail.com>
2025-10-29 16:52:41 -0400

    This is Daniel Lung's Assignment 8. I have added a Containerfile and a script called meow

---
+++ student-work/assignment-8/Daniel-Lung/meow.py
@@ -0,0 +1,12 @@
+def generate_cat():
+    """Prints an ASCII art representation of a cat."""
+    cat_art = r"""
+ /\_/\
+( o.o )
+ > ^ <
+  / \
+"""
+    print(cat_art)
+
+if __name__ == "__main__":
+    generate_cat()
