124ca31ea3e86e243a0346bb56500aef472afc64
YI-CHEN LO (Ejane) <loejane170@gmail.com>
2025-10-29 15:14:31 -0400

    This is my assignment 8. I've added a Containerfile and a script

---
+++ student-work/assignment-8/Ejane-Lo(YI-CHEN-LO)/script.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# script.py
+from datetime import datetime
+
+print("Hello from Assignment 8!")
+print("Author: Ejane Lo (YI-CHEN LO)")
+print("Timestamp:", datetime.utcnow().isoformat() + "Z")
+
