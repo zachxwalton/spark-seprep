3538a1eed40ce55473517973da767e284f2d6ce5
Ze Song <gzsong@bu.edu>
2025-11-01 22:12:05 -0400

    This is my Assignment 8: Added Containerfile and working Docker Hub container

---
+++ student-work/assignment-8/ZE-SONG/hello_container.py
@@ -0,0 +1,7 @@
+"""
+hello_container.py
+Author: Ze Song
+This is a small Python script that prints a funny message
+"""
+
+print("Hello there! This is Ze Song. Unfortunately, I am being trapped inside a container...")
