29d8850af3642f5222ddcc6a067e6819afe37383
Shouqald <Shouqald@bu.edu>
2025-11-05 19:44:05 -0500

    Assignment 8: add Containerfile and motivation script

---
+++ student-work/assignment-8/shoug-aldrees/motivation.py
@@ -0,0 +1,11 @@
+import random
+
+quotes = [
+    "Believe you can and you're halfway there. — Theodore Roosevelt",
+    "It always seems impossible until it's done. — Nelson Mandela",
+    "Success is not final; failure is not fatal. — Winston Churchill",
+    "Keep going. Everything you need will come to you at the perfect time."
+]
+
+print("✨ Here's your motivational quote of the day ✨")
+print(random.choice(quotes))
